7916cf2edb9e4aafe4a2ecf67e67cb39c72a2644
Antonio Jimenez <antojim3@yahoo.com>
2021-02-02 13:14:53 -0600

    color

---
+++ sketchv3.js
@@ -0,0 +1,45 @@
+let hue = 0;
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    //frameRate(30);
+    strokeWeight(0);
+    blendMode(DIFFERENCE);
+    colorMode(HSB,255);
+    
+    
+  }
+  
+  function draw() {
+    
+   
+    background(hue,255,255);
+    hue += 0.5;
+    if (hue > 255) hue = 0;
+ 
+    if (mouseIsPressed) {
+        
+        frameRate(15);
+        //strokeWeight(2);
+        fill(random(0,255),random(0,255),random(0,255),random(50,225));
+        triangle(mouseX, mouseY, random(0,windowWidth), random(0,windowHeight), random(0,windowWidth), random(0,windowHeight));
+        
+      } else {
+        frameRate(10);
+        fill(random(0,255),random(0,255),random(0,255),10);
+        for (let x = 1; x < 40; x++) {
+          for (let y = 1; y < 40; y++) {
+            let ray = random(150,255)
+            stroke(ray, ray,random(50));
+            strokeWeight(random(1));
+            line(mouseX,mouseY, random(0,windowWidth), random(0,windowHeight));
+            
+          }
+        }
+        //strokeWeight(random(5));
+        //triangle(random(0,windowWidth), random(0,windowHeight), random(0,windowWidth), random(0,windowHeight), random(0,windowWidth), random(0,windowHeight));
+      }
+
+  
+  }
+
+ 
