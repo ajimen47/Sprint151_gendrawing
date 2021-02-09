dbb5104a93de6e188dce3aa2523633b951def120
Antonio Jimenez <ajimen47@uic.edu>
2021-02-09 12:52:38 -0600

    Update sketchv3.js
    final

--- sketchv3.js
+++ sketchv3.js
@@ -1,45 +1,61 @@
 let hue = 0;
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    //frameRate(30);
-    strokeWeight(0);
-    blendMode(DIFFERENCE);
-    colorMode(HSB,255);
-    
-    
-  }
+  createCanvas(windowWidth, windowHeight);
+  //frameRate(30);
   
-  function draw() {
-    
-   
-    background(hue,255,255);
-    hue += 0.5;
-    if (hue > 255) hue = 0;
- 
-    if (mouseIsPressed) {
-        
-        frameRate(13);
-        //strokeWeight(2);
-        fill(random(0,255),random(0,255),random(0,255),100);
-        triangle(mouseX, mouseY, random(0,windowWidth), random(0,windowHeight), random(0,windowWidth), random(0,windowHeight));
-        
-      } else {
-        frameRate(10);
-        fill(random(0,255),random(0,255),random(0,255),10);
-        for (let x = 1; x < 40; x++) {
-          for (let y = 1; y < 40; y++) {
-            let ray = random(150,255);
-            stroke(ray, ray,random(50));
-            strokeWeight(1);
-            line(mouseX,mouseY, random(0,windowWidth), random(0,windowHeight));
-            
-          }
-        }
-        //strokeWeight(random(5));
-        //triangle(random(0,windowWidth), random(0,windowHeight), random(0,windowWidth), random(0,windowHeight), random(0,windowWidth), random(0,windowHeight));
+  blendMode(DIFFERENCE);
+  colorMode(HSB, 255);
+
+
+}
+
+function draw() {
+  //blendMode(DIFFERENCE);
+  strokeWeight(0);
+  background(hue, 255, 255);
+  hue += 0.5;
+  if (hue > 255) hue = 0;
+
+  if (mouseIsPressed) {
+
+    frameRate(13);
+    //strokeWeight(2);
+    fill(random(0, 255), random(0, 255), random(0, 255), 70);
+    //erase();
+    triangle(mouseX, mouseY, random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
+    //noErase()
+
+    //erase();
+    //triangle(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
+    noErase();
+
+  } else {
+    frameRate(10);
+    fill(random(0, 255), random(0, 255), random(0, 255), 10);
+    for (let x = 1; x < 40; x++) {
+      for (let y = 30; y < 40; y++) {
+        let ray = random(150, 255);
+        stroke(ray, ray, random(50));
+        strokeWeight(1);
+        line(mouseX, mouseY, random(0, windowWidth), random(0, windowHeight));
+        //erase();
+        //noErase();
+
+
       }
+    }
 
-  
+
+
   }
+  redraw();
+  rectMode(CENTER);
+  fill(random(0, 255), random(0, 255), random(0, 255), 255);
+  strokeWeight(3);
+  noFill();
+  rect(mouseX, mouseY,random(0, windowWidth), random(0, windowHeight));
+  //triangle(random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight), random(0, windowWidth), random(0, windowHeight));
 
- 
+
+}
+
